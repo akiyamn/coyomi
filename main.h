42d9fb51f39d70b691127f7041a8069cd2ab77c9
akiyamn <10993186+akiyamn@users.noreply.github.com>
2021-02-21 22:17:02 +1100

    Refactored main into a header file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+#include <time.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+#include <ncurses.h>
+
+#define DAYS_IN_WEEK 7
+#define DAY_NAME_SIZE 50
+#define COMMAND_SIZE 50
+#define MAIN_TEXT_SIZE 5000
+#define ASCII_DIGIT_START 48
+#define SECONDS_IN_DAY 86400
+#define LAYOUT_X_RATIO 0.25
+#define MAIN_PADDING 2
+
+// Will hard code these for now...
+#define TERMINAL "st"
+#define EDITOR "vim"
+
+#endif
